a2a81c26796be30c2513b4507be9d9e14c91c80e
kurdi.89 <kurdi.89@hotmail.com>
2020-01-15 11:04:05 +0300

    added captions

--- js/app.js
+++ js/app.js
@@ -24,14 +24,19 @@ jQuery(document).ready(async() => {
         ndoes.push(element.node)
     })
     ndoes.forEach(x=>{
+        if(x.edge_media_to_caption.edges[0]){
+            var text = x.edge_media_to_caption.edges[0].node.text;
+        }else{
+            var text = "";
+        }
         var img = `<div class="card-img-top node-image"><img src="${x.thumbnail_src}" width="100%"</div>`
         var div = `
         <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-2 node" id="${x.id}">
             <div class="card m-1 mx-auto" style="width: 14rem;">
                 ${img}
                 <div class="card-body">
-                <h6 class="card-title node-status">video : ${x.is_video}, deminsions : ${x.dimensions.width}x${x.dimensions.height}</h6>
-                <p class="card-text m-0"><small class="text-muted node-id">${x.id}</small></p>
+                <h6 class="card-title node-status">id:${x.id}</h6>
+                <p class="card-text m-0"><small class="text-muted node-id">${text}, video : ${x.is_video}, deminsions : ${x.dimensions.width}x${x.dimensions.height}</small></p>
                 </div>
             </div>
         </div>
